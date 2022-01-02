34443c163181cd11677e13d753a2874d7061f283
Bilaal Yadallee <bilaal.yadallee@insign.fr>
2022-01-02 22:29:58 +0100

    fix length params

--- api/hello.js
+++ api/hello.js
@@ -1,5 +1,5 @@
 export default function handler(request, response) {
     const { name } = request.query;
-    response.status(200).send(`Hello ${name}! There is ${request.query.length} params`);
+    response.status(200).send(`Hello ${name}! There is ${Object.keys(request.query).length} params`);
 }
   
